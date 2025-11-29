8537ba9f3c9897687a33fb36f5ae74d0b5dad9a7
joseph <nwekejoseph>
2025-11-29 19:49:04 +0100

    feat: implement mobile menu toggle functionality and update UI

--- church-audio-based-web-app/src/app/main/home/home.ts
+++ church-audio-based-web-app/src/app/main/home/home.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
@@ -8,5 +8,24 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
   styleUrl: './home.scss',
 })
 export class Home {
+  isMobileMenuOpen = false;
+  @ViewChild('mobileMenu') mobileMenu!: ElementRef;
+  @ViewChild('menuToggle') menuToggle!: ElementRef;
 
+  toggleMobileMenu() {
+    this.isMobileMenuOpen = !this.isMobileMenuOpen;
+  }
+
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: MouseEvent) {
+    if (
+      this.isMobileMenuOpen &&
+      this.mobileMenu &&
+      this.menuToggle &&
+      !this.mobileMenu.nativeElement.contains(event.target) &&
+      !this.menuToggle.nativeElement.contains(event.target)
+    ) {
+      this.isMobileMenuOpen = false;
+    }
+  }
 }
